40f568184c5de35edfe8c8abf6cf1d7fe2ab97f3
xingoxu <xingoxu@gmail.com>
2016-06-28 09:10:17 +0800

    release 0.0.1

---
+++ main.js
@@ -0,0 +1,70 @@
+//输入line表情地址
+//line sticker url here
+var url = "https://store.line.me/stickershop/product/1244248/ja"; 
+//输入文件夹名字，为空则下载到当前文件夹下的images文件夹，最好不要使用中文
+//FolderName,If it is null or "" then it will use "images"
+var folderName = ""; 
+
+(function() {
+  var httpClient = require('request'),
+    domEnv = require('jsdom').env,
+    jQueryModule = require('jquery'),
+    fs = require('fs');
+
+  function getLinePage(resolve, reject) {
+    console.log('Getting WebPage...');
+    httpClient.get(url, function(err, response, body) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      domEnv(body, function(errors, window) {
+        if (errors) {
+          reject(errors);
+          return;
+        }
+        var $ = jQueryModule(window);
+        console.log('Getting StickerID...');
+        var firstAndLastNode = Array.prototype.slice.call($('ul.FnSticker_animation_list_img li:first-child span,ul.FnSticker_animation_list_img li:last-child span'));
+        var imageURL = ""; //url前缀
+        firstAndLastNode.forEach(function(value, index, array) {
+          var backgroundImage = value.style.backgroundImage,
+            lastIndex = backgroundImage.lastIndexOf('/');
+          imageURL = backgroundImage.substring(backgroundImage.indexOf('http'), lastIndex + 1);
+          firstAndLastNode[index] = backgroundImage.substring(lastIndex + 1, backgroundImage.lastIndexOf('.'));
+        })
+
+        resolve({
+          first: Number.parseInt(firstAndLastNode[0]),
+          last: Number.parseInt(firstAndLastNode[1]),
+          url: imageURL
+        });
+      })
+    })
+  }
+
+  (new Promise(getLinePage))
+  .then(function(ids) {
+      'use strict';
+      if (ids.first > ids.last) {
+        let temp = first;
+        first = last;
+        last = temp;
+      }
+
+      if (!folderName||folderName == '')
+        folderName = 'images';
+
+      console.log('Downloading...');
+
+      fs.mkdirSync(folderName);
+
+      for (var i = ids.first; i <= ids.last; i++) {
+        httpClient(ids.url + i + '.png').pipe(fs.createWriteStream(folderName + '/' + i + '.png'))
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+
+})();
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Node Line Sticker Downloader
+
+# How to use
+
+1. install [Node.js](https://nodejs.org/)
+2. Download the [repo](https://github.com/xingoxu/line-sticker-downloader/archive/master.zip)
+3. Use bash or cmd into your repo
+4. If you use it for first time, enter the following words in bash/cmd.
+```
+npm install
+```
+5. Modify the line sticker url and folderName in main.js.
+6. Enter the following words in bash/cmd.
+```
+node main
+```
+7. That's all.
